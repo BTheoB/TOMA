25078e59f5dbf547178722176e9c0b81da2403f5
Bastienne-Banco Théo <theo.bastienne-banco@etu.univ-lyon1.fr>
2022-11-25 15:31:52 +0100

    add fonction de recuperation de données de parking

--- src/App.js
+++ src/App.js
@@ -78,63 +78,37 @@ function ApiTRP() {
   }
 
   return (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>{content}</p>
-    </header>
-    <Map />
-  </div>
-  );       
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{content}</p>
+      </header>
+      <Map />
+    </div>
+  );
 }
 
-//function test d'affichage des infos completes sur les parkings 
-function GetAdressParking() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
+//  Cette fonction créer un tableau tel que tab(key,value) nommé FormatedTabParkingAdress 
+//  avec la key étant l'identifier (ex "L23DL4") et la valeur etant les informations sur le parking
+//  Cela permet de retrouver plus facilement les informations d'un parking
+function setTabParking(parkingsInfo){
+  
+  let formatedparkingsInfo = {};
+  parkingsInfo.values.forEach((element) => {                   //Créer un tableau permettant d'acceder directement à l'élement voulue par ca clé
+    formatedparkingsInfo[element["identifier"]] = element;
+  });
 
-  // Remarque : le tableau vide de dépendances [] indique
-  // que useEffect ne s’exécutera qu’une fois, un peu comme
-  // componentDidMount()
-  useEffect(() => {
-    fetch("https://download.data.grandlyon.com/ws/rdata/lpa_mobilite.parking_lpa_2_0_0/all.json?maxfeatures=100&start=1")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result["values"]["address"]);
-        },
-        // Remarque : il faut gérer les erreurs ici plutôt que dans
-        // un bloc catch() afin que nous n’avalions pas les exceptions
-        // dues à de véritables bugs dans les composants.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, []);
+  localStorage.setItem('FormatedTabParkingInfo', JSON.stringify(formatedparkingsInfo));
 
-  let content = ' ';
+}
 
+//fonction qui rend les information d'un parking selon son Identifiant 
+function GetInfoParking(id) {
+  
+  let FormatedTabParkingAdress = localStorage.getItem('FormatedTabParkingInfo');
+  // Il faut surment parse la string FormatedTabParkingAdress
+  return FormatedTabParkingAdress[id];
 
-  if (error) {
-    content = <div>Erreur : {error.message}</div>
-
-  } else if (!isLoaded) {
-    content = <div>Chargement...</div>
-  } else {
-    content = items
-  }
-
-
-
-  return (<div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      {content}
-    </header>
-  </div>);
 }
 
 //function test d'affichages des info sur les parkings avec leur nombre de place en temps réel
@@ -153,6 +127,7 @@ function GetParkingsRealTime() {
         (result) => {
           setIsLoaded(true);
           setItems(result);
+          localStorage.setItem('resulatParkingsRealTime', JSON.stringify(result));
         },
         // Remarque : il faut gérer les erreurs ici plutôt que dans
         // un bloc catch() afin que nous n’avalions pas les exceptions
@@ -191,8 +166,9 @@ function GetParkingsInfo() {
         (result) => {
           setIsLoaded(true);
           setItems(result);
-          localStorage.setItem('resulatParkings', JSON.stringify(result));
-          },
+          localStorage.setItem('resulatParkingsInfo', JSON.stringify(result));
+          setTabParking(result);
+        },
         // Remarque : il faut gérer les erreurs ici plutôt que dans
         // un bloc catch() afin que nous n’avalions pas les exceptions
         // dues à de véritables bugs dans les composants.
@@ -207,7 +183,7 @@ function GetParkingsInfo() {
     return error.message
   } else if (!isLoaded) {
     return "Chargement..."
-  } else{
+  } else {
     return (
       <ul>
         {items.values.map(item => (
@@ -220,6 +196,8 @@ function GetParkingsInfo() {
   }
 
 }
+
+
 
 
 
@@ -259,7 +237,7 @@ function App() {
 
   initContext();
 
-  var items = localStorage.getItem('resulatParkings') ? localStorage.getItem('resulatParkings'): [];
+  var items = localStorage.getItem('resulatParkingsInfo') ? localStorage.getItem('resulatParkingsInfo') : [];
   items = JSON.parse(items);
   console.log(items);
   return (
