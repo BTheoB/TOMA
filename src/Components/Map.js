3be44745e6e2cabbc0703d4260adeadc6e25e8b2
OmarRaslan99 <53241361+OmarRaslan99@users.noreply.github.com>
2022-12-14 20:34:09 +0100

    fixer le pb de map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -64,19 +64,18 @@ export class MapContainer extends Component {
 
           
             {/* <Marker key={"LPA0764"} position={{ lat: 45.768522, lng: 4.83773 }} onClick={() => {parkingInfoPopUp("LPA0764")}} /> */}
-            {loadMarkerParkingMap().length == 0 ? loadMarkerParkingMap().map(parking =>(
+            {/* {loadMarkerParkingMap().length == 0 ? loadMarkerParkingMap().map(parking =>(
               <Marker key={parking["id"]} position={{ lat: parking["lat"], lng: parking["lon"] }} onClick={() => {this.parkingInfoPopUp(parking["id"])}} />
-            )) : true}
+            )) : true} */}
+            {loadMarkerParkingMap().map(parking =>(
+              // <Marker key={parking["id"]} position={{ lat: parking["lat"], lng: parking["lon"] }} onClick={() => {parkingInfoPopUp(parking["id"])}} />
+              <Marker key={parking["id"]} position={{ lat: parking["lat"], lng: parking["lon"] }} onClick={() => {this.parkingInfoPopUp(parking["id"])}} />
+            ))}
           </Map>
         </div>
       </div>
-
-
     );
   }
-
-  
-
 }
 
 
